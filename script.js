990801e5d685db2909c6abd70703ba2bd4eb93d7
Siddhesh Rao <119157669+siddhesh-rao@users.noreply.github.com>
2025-01-31 16:31:20 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('wishButton').addEventListener('click', function() {
+    const surpriseMessage = document.getElementById('surpriseMessage');
+    surpriseMessage.classList.toggle('hidden');
+});
